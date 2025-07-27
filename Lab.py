86de9051002faa4ab42df8915dc085dc8676925e
hasaben03 <hasaben03@gmail.com>
2025-07-28 00:15:14 +0100

    Create Lab.py

---
+++ Lab.py
@@ -0,0 +1,158 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "de4df0fd-2350-479c-95c2-6db180e2c28f",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "32439a7d-2bce-4bf7-84ea-742deef3be3b",
+   "metadata": {},
+   "source": [
+    "# Data Science Tools and Ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "74244748-d8f7-4c97-85a5-1c3dc2983862",
+   "metadata": {},
+   "source": [
+    "In this notebook, Data Science Tools and Ecosystem are summarized."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "ce22c55e-e8aa-4bbf-9894-10a07382de3a",
+   "metadata": {},
+   "source": [
+    "Some of the popular languages that Data Scientists use are:\n",
+    "1. Python\n",
+    "2. R\n",
+    "3. SQL"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "2007c265-81fc-46cd-88bb-4ed45560c29a",
+   "metadata": {},
+   "source": [
+    "Some of the commonly used libraries used by Data Scientists include:\n",
+    "1. Pandas\n",
+    "2. NumPy\n",
+    "3. Matplotlib"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b3d87e79-923e-4131-a986-aca598a43f97",
+   "metadata": {},
+   "source": [
+    "| Data Science Tools |\n",
+    "|--------------------|\n",
+    "| Jupyter Notebook   |\n",
+    "| RStudio            |\n",
+    "| Apache Zeppelin    |"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0a98d95d-f8b0-4324-beb6-c50d0b9bd1ff",
+   "metadata": {},
+   "source": [
+    "### Below are a few examples of evaluating arithmetic expressions in Python."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "d1f0fcb9-2340-4d8c-998e-cc4bc688fb98",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "17"
+      ]
+     },
+     "execution_count": 1,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# This is a simple arithmetic expression\n",
+    "to multiply then add integers\n",
+    "(3 * 4) + 5"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "c53e5263-90d3-4f85-8880-6bbec0a23a4c",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "3.3333333333333335"
+      ]
+     },
+     "execution_count": 2,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# This will convert 200 minutes to hours by dividing by 60\n",
+    "200 / 60\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "96563d73-6520-4d4f-8e41-630ecaa4e599",
+   "metadata": {},
+   "source": [
+    "**Objectives:**\n",
+    "\n",
+    "- List popular languages for Data Science\n",
+    "- Identify commonly used libraries\n",
+    "- Create and format Markdown in Jupyter\n",
+    "- Perform basic arithmetic expressions\n",
+    "- Share notebooks through GitHub"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "3b376f6f-fecb-4cca-aecb-02a2a9239931",
+   "metadata": {},
+   "source": [
+    "## Author\n",
+    "\n",
+    "Hazanul Banna"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
